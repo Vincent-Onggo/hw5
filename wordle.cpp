158c382babe1594b752c4abd7478db9df7cb9d43
Vincent O <vincentonggo@gmail.com>
2024-04-14 11:17:14 -0700

    changed approach

--- wordle.cpp
+++ wordle.cpp
@@ -11,12 +11,8 @@
 #include "dict-eng.h"
 using namespace std;
 
-char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-// Add prototypes of helper functions here
-void tryCharacter(string, string, set<string>&, int, int, const std::set<std::string>& );
-void generatePossibleSol(string, string, set<string>&, const std::set<std::string>& );
-bool floatingCheck(string in, string floating);
-
+void permutateFloating(string in, const string& floating, set<string>& fixed, size_t index);
+void tryCharacters(string in, size_t index, set<string>& sol, const set<string>& dict);
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -25,48 +21,45 @@ std::set<std::string> wordle(
 {
     // Add your code here
     std::set<std::string> sol;
-    generatePossibleSol(in, floating, sol, dict);
+    std::set<std::string> fixed;
+    permutateFloating(in, floating, fixed, 0);
+    for(set<string>::iterator it = fixed.begin(); it != fixed.end(); ++it){
+        tryCharacters(*it,  0, sol, dict);
+    }
     return sol;
 
 }
 
 // Define any helper functions here
-void tryCharacter(string in, string floating, set<string>& sol, int blank_index, int letter, const std::set<std::string>& dict){
-    if(letter == 26){
+void tryCharacters(string in, size_t index, set<string>& sol, const set<string>& dict){
+    if(index == in.length()){
+        if(dict.find(in) != dict.end()){
+            sol.insert(in);
+        }
         return;
+    }
+    if(in[index] != '-'){
+        tryCharacters(in, index + 1, sol, dict);
     }else{
-        string newWord_lower, newWord_upper;
-        newWord_lower = newWord_upper = in;
-        newWord_lower[blank_index] = letters[letter];
-        generatePossibleSol(newWord_lower, floating, sol, dict);
-        letter++;
-        tryCharacter(in, floating, sol, blank_index, letter, dict);
+        for(int i=0; i<26; i++){
+            string newString = in;
+            newString[index] = 'a' + i;
+            tryCharacters(newString, index + 1, sol, dict);
+        }
     }
 }
 
-void generatePossibleSol(string in, string floating,  set<string>& sol, const std::set<std::string>& dict){
-    for(int i=0; i<in.length(); i++){
-        if(in[i] == '-'){
-            tryCharacter(in, floating, sol, i, 0, dict);
-        }
+void permutateFloating(string in, const string& floating, set<string>& fixed, size_t index) {
+    if (index == floating.length()) {
+        fixed.insert(in);
+        return;
     }
-    if(dict.find(in) != dict.end() and floatingCheck(in, floating)){
-        sol.insert(in);
-    }
-}
 
-bool floatingCheck(string in, string floating){
-    for(int i=0; i<floating.length(); i++){
-        bool containsLetter = false;
-        for(int j=0; j<in.length(); j++){
-            if(in[j] == floating[i]){
-                in[j] = ' ';
-                containsLetter = true;
-            }
-        }
-        if(!containsLetter){
-            return false;
+    for (size_t i = 0; i < in.length(); ++i) {
+        if (in[i] == '-') {
+            in[i] = floating[index];
+            permutateFloating(in, floating, fixed, index + 1);
+            in[i] = '-';
         }
     }
-    return true;
 }
